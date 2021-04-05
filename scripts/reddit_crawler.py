58cb04d94a045b0697719700c3458b51c7c5ddd1
Cesard97 <44006906+Cesard97@users.noreply.github.com>
2021-04-05 01:10:07 -0500

    Finished crawler for english subreddits

--- scripts/reddit_crawler.py
+++ scripts/reddit_crawler.py
@@ -1,10 +1,14 @@
 import requests
 import os
 import json
+import time
 
 # Credentials
 CLIENT_ID = 'X2j9MFeI8m1BlQ'
 SECRET_KEY = 'u3AEGdbN2aCzcS4dhK1CKAEwGufxGg'
+
+# Global Variable
+doc_id = 0
 
 def create_headers():
     """ Creates request header with authorization token
@@ -40,37 +44,82 @@ def create_headers():
 
     return headers
 
-def retrieve_subreddit_posts(headers, subreddit=['COVID19']):
+def retrieve_subreddit_posts(headers, subreddit='COVID19', filter_flair=None, last_id=None, lang='english'):
     """ Retrieves the text and additional from the specificed subreddit
     Args:
-        headers (dict): headers with authorization token to access reddit API
-        subreddit (str): The subreddit wanted to be access
+        headers (dict): headers with authorization token to access reddit API.
+        subreddit (str): The subreddit where the posts will be retrieved.
+        filter_flair (str): flair to filter the posts by.
+        last_id (str): The ID of the last post retrieved.
+        lang (str): Language of the subreddit.
     Returns:
-        json_response (dict): headers with authorization token to access reddit API
+        json_response (dict): headers with authorization token to access reddit API.
     """
-    # Get all the hot posts from the specificed subreddit
-    res = requests.get('https://oauth.reddit.com/r/{}/new'.format(subreddit[0]),
-          headers=headers, params={'limit':'100'})
+    global doc_id
+    # Get last 100 the hot posts from the specificed subreddit
+    if last_id is None:
+        res = requests.get('https://oauth.reddit.com/r/{}/new'.format(subreddit),
+                            headers=headers, params={'limit':'100'})
+    else:
+        res = requests.get('https://oauth.reddit.com/r/{}/new'.format(subreddit),
+                        headers=headers, params={'limit':'100', 'after':last_id})
+
     #res = requests.get('https://oauth.reddit.com/r/python/hot', headers=headers)
 
     # Retrieve only relevant info from last 100 posts
-    #print(len(res.json()['data']['children']))
+    if len(res.json()['data']['children']) == 0:
+        return ''
+
     for post in res.json()['data']['children']:
-        #print(post['data'].keys())
-        doc = {
-            'source' : 'Reddit_'+post['data']['subreddit'],
-            'id':post['kind'] + '_' + post['data']['id'],
-            'author':post['data']['author_fullname'],
-            'text':post['data']['title'] + post['data']['selftext'],
-            'url':post['data']['url'],
-            'lang':'english'
-            }
-        print(doc)
+        try:
+            doc = {
+                'source' : 'Reddit',
+                'subreddit' : post['data']['subreddit'],
+                'id':post['kind'] + '_' + post['data']['id'],
+                'author':post['data']['author_fullname'],
+                'text':post['data']['title'] + ' ' + post['data']['selftext'],
+                'url':post['data']['url'],
+                'lang':lang
+                }
+            # Save the post
+            with open('docs/REDDIT_{:05d}.json'.format(doc_id), 'w') as fp:
+                json.dump(doc, fp, sort_keys=True, indent=4)
+            doc_id += 1
+        except:
+            print('Error retrieving post')
+
+    # Get last id
+    new_last_id = doc['id']
+    time.sleep(1)
+    # Return last ID to track
+    return new_last_id
 
 def main():
     headers = create_headers()
-    retrieve_subreddit_posts(headers)
-    #print(json.dumps(json_response, indent=4, sort_keys=True))
+    # Define English Subreddits
+    eng_subreddits = ['COVID19', 'COVID', 'Coronavirus', 'COVID19positive', 'China_Flu', 'COVID19_support', 'CoronavirusCanada']
+    # First iteration
+    eng_last_ids = ['', '', '', '', '', '', '']
+    for i, subreddit in enumerate(eng_subreddits):
+        eng_last_ids[i] = retrieve_subreddit_posts(headers, subreddit=subreddit)
+
+    # Continous loop
+    docs_num = len(eng_subreddits)*100
+    while True:
+        for i, subreddit in enumerate(eng_subreddits):
+            # Retrieve english posts
+            eng_last_ids[i] = retrieve_subreddit_posts(headers,
+                                                       subreddit=subreddit,
+                                                       last_id=eng_last_ids[i])
+
+        # Delete subreddit if it reaches last post
+        for i, ids in enumerate(eng_last_ids):
+            if ids == '':
+                eng_last_ids.pop(i)
+                eng_subreddits.pop(i)
+
+        docs_num += len(eng_subreddits)*100
+        print('Added new posts! - Total Posts: {} (Approx)'.format(docs_num))
 
 
 if __name__ == "__main__":
