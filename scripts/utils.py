49b28dfa9bcf92172aceb9b72db2dd20287d70f6
JuanDavidG1997 <jd.garciah@uniandes.edu.co>
2021-05-21 17:10:56 -0500

    Started tweet tagging notebook

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,13 +2,16 @@ import numpy as np
 import os, json, nltk, random, time
 from gensim.parsing.porter import PorterStemmer
 from tqdm import tqdm
+import string
 import concurrent.futures
+from nltk.tokenize.casual import TweetTokenizer
 
 
 class Utils:
     def __init__(self, path_prefix, num_workers = 15):
         self.path_prefix = path_prefix
         self.num_workers = num_workers
+        self.tweet_tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=True)
 
     def data_loader(self, lang, source, total_data=None):
         """ Function to retrieve tweet data in specified language
@@ -22,7 +25,7 @@ class Utils:
         start = time.time()
         def chunk_loader(path, files_list):
             data = []
-            for file in tqdm(files_list):
+            for file in files_list:
                 with open(os.path.join(path, file), 'r+') as file_str:
                     data_dict = json.load(file_str)
                     data.append(data_dict['text'])
@@ -50,62 +53,40 @@ class Utils:
         return data
 
     def preprocessing(self, text,
-                            remove_contractions = False,
-                            remove_stop_words = False,
-                            stemming = False,
-                            tonekize = False,
-                            lemmatize = False,
-                            tweet_tokenizer = False):
+                        stop_words = None,
+                        stemmer = None,
+                        tokenizer = None,
+                        lemmatizer = None):
         """ Function to apply preprocessing steps to text
 
         Args:
             text (str): String to preprocess
-            remove_contractions (bool): True to apply contractions removal
-            remove_stop_words (bool): True to remove stop words
-            stemming (bool): True to apply stemming
-            tokenize (bool): True to tokenize text
-            lemmatize (bool): True to lemmatize
-            tweet_tokenizer (bool): True to use nltk tweet tokenizer
+            stop_words (list): stopwords to remove
+            stemmer (nltk.stem): stemmer to be used
+            tokenizer (nltk.tokenize): tokenizer to be used
+
 
         Returns:
             str: Preprocessed string
         """
+        # Stems text
+        if stemmer:
+            text = stemmer.stem(text)
 
-        if tweet_tokenizer:
-            tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=True)
-        else:
-            tokenizer = nltk.RegexpTokenizer(r'\w+')
-
-        porter_stemmer = PorterStemmer()
-        lemmatizer = nltk.stem.WordNetLemmatizer()
-
-        # Converts to lowercase
-        text = text.lower()
-
-        # Replace colloquial terms
-        if remove_contractions:
-            for word in text.split():
-                if word in list(contraction_colloq_dict.keys()):
-                    text = text.replace(word, contraction_colloq_dict[word])
+        # # Converts to lowercase
+        text = "".join([char.lower() for char in text if char not in string.punctuation])
 
         # Removes stopwords
-        if remove_stop_words:
-            text = remove_stopwords(text)
-
-        # Stems text
-        if stemming:
-            text = porter_stemmer.stem_sentence(text)
-
-        # Tokenizes text and removes punctuation
-        if tokenize:
+        if tokenizer:
             text = tokenizer.tokenize(text)
 
-        # Lemmatizes text
-        if lemmatize:
-            text_lm = []
-            for word in text:
-               text_lm.append(self.lemmatizer.lemmatize(word))
-        text = text_lm
 
+        # Lemmatizes text
+        relevant_words=[]
+        for word in text:
+            if word not in stop_words:
+                relevant_words.append(word)
+
+        text = relevant_words
         # Returns preprocessed text
         return text
